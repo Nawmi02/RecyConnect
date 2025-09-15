a24d280796f6c4fdb79f14cc98ead3ec55ffd6ff
lubna-219 <lubnaakter12388@gmail.com>
2025-09-15 14:09:38 +0600

    Notification model Done

--- Notifications/models.py
+++ Notifications/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Notifications(models.Model):
+    text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=True)
+    icon = models.CharField(max_length=20)
+    is_read = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.text
