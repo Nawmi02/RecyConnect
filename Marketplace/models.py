e08c019453cad5544695b8bc57ca35326ad88d22
unknown <njahanurmi3@gmail.com>
2025-09-15 13:13:50 +0600

    create marketplace model

--- Marketplace/models.py
+++ Marketplace/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Marketplace(models.Model):
+    name = models.CharField(max_length=200)
+    grade = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    description = models.TextField(blank=True , null=True)
+    location = models.CharField(max_length=200)
+    weight = models.DecimalField(max_digits=6 , decimal_places=2)
+    price = models.DecimalField(max_digits=6 , decimal_places=2)
+
+
+    def __str__(self):
+        return self.name
+
+
