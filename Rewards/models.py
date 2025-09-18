650f66cc9d30618ad79744d531653c0d7dbc0f39
unknown <njahanurmi3@gmail.com>
2025-09-18 21:56:09 +0600

    created reward model

--- Rewards/models.py
+++ Rewards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Reward(models.Model):
+    name = models.CharField(max_length=200)                     
+    description = models.TextField(blank=True, null=True)       
+    image = models.ImageField(upload_to="rewards/", blank=True, null=True) 
+    points_required = models.IntegerField()
+    redeemed_at = models.DateTimeField(blank=True, null=True)                      
+
+    def __str__(self):
+        return self.name
