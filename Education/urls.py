2cf989ab239de9697124b37f9b8e09007d053f63
anamika-hossain <anamikahossain837@gmail.com>
2025-09-23 00:39:50 +0600

    Learn URLS UPD

--- Education/urls.py
+++ Education/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name = "education"
+
 urlpatterns = [
-    path('education-awareness/', views.education_awareness, name='education_awareness'),
+    path("", views.education_awareness, name="education_awareness"),
+
+    # Guides/Articles (PDF)
+    path("<int:pk>/pdf/view/", views.view_guide_pdf, name="view_guide_pdf"),
+    path("<int:pk>/pdf/download/", views.download_guide_pdf, name="download_guide_pdf"),
+
+    # Videos
+    path("<int:pk>/video/view/", views.view_video, name="view_video"),
+    path("<int:pk>/video/download/", views.download_video, name="download_video"),
 ]
